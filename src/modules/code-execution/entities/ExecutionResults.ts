c547a15de0e6291d92fb3157fd197eac008b55b3
kiishi-0 <kiishioluwa@gmail.com>
2025-07-20 22:15:47 +0100

    Moved entities to their appropriate modules

---
+++ src/modules/code-execution/entities/ExecutionResults.ts
@@ -0,0 +1,51 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { CodeSubmissions } from './CodeSubmissions';
+import { Sessions } from '../../../entities/entities/Sessions';
+import { Users } from '../../user/entities/Users';
+
+@Index('execution_results_pkey', ['id'], { unique: true })
+@Entity('execution_results', { schema: 'public' })
+export class ExecutionResults {
+  @Column('uuid', { primary: true, name: 'id' })
+  id: string;
+
+  @Column('text', { name: 'stdout', nullable: true })
+  stdout: string | null;
+
+  @Column('text', { name: 'stderr', nullable: true })
+  stderr: string | null;
+
+  @Column('integer', { name: 'exit_code' })
+  exitCode: number;
+
+  @Column('double precision', { name: 'time_taken_ms', precision: 53 })
+  timeTakenMs: number;
+
+  @Column('timestamp with time zone', { name: 'created_at' })
+  createdAt: Date;
+
+  @OneToMany(() => CodeSubmissions, (codeSubmissions) => codeSubmissions.result)
+  codeSubmissions: CodeSubmissions[];
+
+  @ManyToOne(
+    () => CodeSubmissions,
+    (codeSubmissions) => codeSubmissions.executionResults,
+  )
+  @JoinColumn([{ name: 'code_submission_id', referencedColumnName: 'id' }])
+  codeSubmission: CodeSubmissions;
+
+  @ManyToOne(() => Sessions, (sessions) => sessions.executionResults)
+  @JoinColumn([{ name: 'session_id', referencedColumnName: 'id' }])
+  session: Sessions;
+
+  @ManyToOne(() => Users, (users) => users.executionResults)
+  @JoinColumn([{ name: 'user_id', referencedColumnName: 'id' }])
+  user: Users;
+}
---
+++ src/modules/user/entities/Users.ts
@@ -0,0 +1,66 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { CodeSnapshots } from '../../code-execution/entities/CodeSnapshots';
+import { CodeSubmissions } from '../../code-execution/entities/CodeSubmissions';
+import { EditorEventLogs } from '../../editor/entities/EditorEventLogs';
+import { ExecutionResults } from '../../code-execution/entities/ExecutionResults';
+import { SessionParticipants } from '../../session/entities/SessionParticipants';
+import { Sessions } from '../../session/entities/Sessions';
+import { Roles } from './Roles';
+
+@Index('users_email_key', ['email'], { unique: true })
+@Index('users_pkey', ['id'], { unique: true })
+@Entity('users', { schema: 'public' })
+export class Users {
+  @Column('uuid', { primary: true, name: 'id' })
+  id: string;
+
+  @Column('character varying', { name: 'email', unique: true, length: 255 })
+  email: string;
+
+  @Column('text', { name: 'password_hash', nullable: true })
+  passwordHash: string | null;
+
+  @Column('character varying', { name: 'firstname', length: 100 })
+  firstname: string;
+
+  @Column('character varying', { name: 'lastname', length: 100 })
+  lastname: string;
+
+  @Column('text', { name: 'avatar_url', nullable: true })
+  avatarUrl: string | null;
+
+  @OneToMany(() => CodeSnapshots, (codeSnapshots) => codeSnapshots.user)
+  codeSnapshots: CodeSnapshots[];
+
+  @OneToMany(() => CodeSubmissions, (codeSubmissions) => codeSubmissions.user)
+  codeSubmissions: CodeSubmissions[];
+
+  @OneToMany(() => EditorEventLogs, (editorEventLogs) => editorEventLogs.user)
+  editorEventLogs: EditorEventLogs[];
+
+  @OneToMany(
+    () => ExecutionResults,
+    (executionResults) => executionResults.user,
+  )
+  executionResults: ExecutionResults[];
+
+  @OneToMany(
+    () => SessionParticipants,
+    (sessionParticipants) => sessionParticipants.user,
+  )
+  sessionParticipants: SessionParticipants[];
+
+  @OneToMany(() => Sessions, (sessions) => sessions.host)
+  sessions: Sessions[];
+
+  @ManyToOne(() => Roles, (roles) => roles.users)
+  @JoinColumn([{ name: 'role_code', referencedColumnName: 'code' }])
+  roleCode: Roles;
+}
